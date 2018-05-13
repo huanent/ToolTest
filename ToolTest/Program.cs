33f28a4d662cedd4fcd03e946a105f42199c6cff
huanent <huanent@hotmail.com>
2018-05-13 23:35:05 +0800

    添加项目文件。

---
+++ ToolTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ToolTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
